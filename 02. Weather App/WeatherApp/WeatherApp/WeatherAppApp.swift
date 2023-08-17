2fe6ebc29edecd1a429a09ab308cd37c0242d89f
Navishka Kularathna <navishka.randika@gmail.com>
2023-08-18 01:51:15 +0800

    Weather app initial commit

---
+++ 02. Weather App/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by user236509 on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
